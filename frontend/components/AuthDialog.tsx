8cd6b27c6121ddf2d4a9f8096411bbde3cf5ec21
ylmzchdr <82415102+ylmzchdr@users.noreply.github.com>
2026-06-21 11:57:21 +0300

    fix google login url

--- frontend/components/AuthDialog.tsx
+++ frontend/components/AuthDialog.tsx
@@ -155,10 +155,10 @@ export default function AuthDialog({ activeTab, onClose }: AuthDialogProps) {
   };
 
 
-  const handleSocialLogin = () => {
-  window.location.href = "http://localhost:5001/auth/google";
+const handleSocialLogin = () => {
+  window.location.href =
+    "https://egelove-backend.onrender.com/auth/google";
 };
-
 
 
 return (
