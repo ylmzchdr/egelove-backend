85702d7c412bb75dc1f137efdd2cc19025994272
ylmzchdr <82415102+ylmzchdr@users.noreply.github.com>
2026-06-26 12:55:55 +0300

    Add Google Analytics tracking

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+
 import Providers from "@/components/Providers";
 import CookieConsent from "@/components/CookieConsent";
 import "./globals.css";
@@ -21,6 +23,19 @@ export default function RootLayout({
           {children}
           <CookieConsent />
         </Providers>
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NX31XDZBXB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NX31XDZBXB');
+          `}
+        </Script>
       </body>
     </html>
   );
