1c94d5ebab9c9000b539f27b7bd82d30b151a302
ylmzchdr <82415102+ylmzchdr@users.noreply.github.com>
2026-09-09 17:28:37 +0300

    refactor: simplify initial country list for MVP

--- international-dev/database/seed/countries.seed.ts
+++ international-dev/database/seed/countries.seed.ts
@@ -1,9 +1,4 @@
-export type CountrySeed = {
-  code: string;
-  name: string;
-};
-
-export const countriesSeed: CountrySeed[] = [
+export const countriesSeed = [
   {
     code: "TR",
     name: "Türkiye",
@@ -17,35 +12,15 @@ export const countriesSeed: CountrySeed[] = [
     name: "İngiltere",
   },
   {
-    code: "NL",
-    name: "Hollanda",
-  },
-  {
-    code: "FR",
-    name: "Fransa",
-  },
-  {
-    code: "BE",
-    name: "Belçika",
-  },
-  {
-    code: "AT",
-    name: "Avusturya",
-  },
-  {
-    code: "CH",
-    name: "İsviçre",
-  },
-  {
-    code: "AZ",
-    name: "Azerbaycan",
-  },
-  {
     code: "RU",
     name: "Rusya",
   },
   {
     code: "UA",
     name: "Ukrayna",
+  },
+  {
+    code: "AZ",
+    name: "Azerbaycan",
   },
 ];
